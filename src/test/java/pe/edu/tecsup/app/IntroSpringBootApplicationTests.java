d9fbfc1fb6ebae34ff2ea5bc6aca98bc27e31ac4
jgomezz <jgomezz@gmail.com>
2026-01-11 10:44:48 -0500

    update

--- src/test/java/pe/edu/tecsup/app/IntroSpringBootApplicationTests.java
+++ src/test/java/pe/edu/tecsup/app/IntroSpringBootApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CpeJavaMod03S01IntroSbApplicationTests {
+class IntroSpringBootApplicationTests {
 
 	@Test
 	void contextLoads() {
