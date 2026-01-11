d55fb49d3f8ae609e3c0b6b373c0ee61e8b65ada
jgomezz <jgomezz@gmail.com>
2026-01-11 09:43:24 -0500

    first commit

---
+++ src/main/java/pe/edu/tecsup/app/IntroSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroSpringBootApplication.class, args);
+	}
+
+}
