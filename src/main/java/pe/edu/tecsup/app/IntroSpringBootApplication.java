34cf2edc7bae472522ed15934e379c8fdb4bb0b3
jgomezz <jgomezz@gmail.com>
2026-01-11 09:54:50 -0500

    update

--- src/main/java/pe/edu/tecsup/app/IntroSpringBootApplication.java
+++ src/main/java/pe/edu/tecsup/app/IntroSpringBootApplication.java
@@ -1,13 +1,23 @@
 package pe.edu.tecsup.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroSpringBootApplication {
+public class IntroSpringBootApplication  implements CommandLineRunner {
+
+    private static Logger log = LoggerFactory.getLogger(IntroSpringBootApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroSpringBootApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        log.info("Hola mundo desde Spring Boot");
+    }
 }
